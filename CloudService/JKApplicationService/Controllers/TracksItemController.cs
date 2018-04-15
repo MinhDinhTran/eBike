0bf9625845d8c642e42233bd27071d54cbd991ae
Jonas <jonas.kibildis@gmail.com>
2018-04-15 15:29:55 +0300

    added cloud service

---
+++ CloudService/JKApplicationService/Controllers/TracksItemController.cs
@@ -0,0 +1,52 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.OData;
+using Microsoft.Azure.Mobile.Server;
+using JKApplicationService.DataObjects;
+using JKApplicationService.Models;
+
+namespace JKApplicationService.Controllers
+{
+    public class TracksItemController : TableController<TracksItem>
+    {
+        protected override void Initialize(HttpControllerContext controllerContext)
+        {
+            base.Initialize(controllerContext);
+            JKApplicationContext context = new JKApplicationContext();
+            DomainManager = new EntityDomainManager<TracksItem>(context, Request);
+        }
+
+        // GET tables/TracksItem
+        public IQueryable<TracksItem> GetAllTracksItems()
+        {
+            return Query();
+        }
+
+        // GET tables/TracksItem/
+        public SingleResult<TracksItem> GetTracksItem(string id)
+        {
+            return Lookup(id);
+        }
+
+        // PATCH tables/Tracks
+        public Task<TracksItem> PatchTracksItem(string id, Delta<TracksItem> patch)
+        {
+            return UpdateAsync(id, patch);
+        }
+
+        // POST tables/TracksItem
+        public async Task<IHttpActionResult> PostTracksItem(TracksItem item)
+        {
+            TracksItem current = await InsertAsync(item);
+            return CreatedAtRoute("Tables", new { id = current.Id }, current);
+        }
+
+        // DELETE tables/TracksItem/48D68C86-6EA6-4C25-AA33-223FC9A27959
+        public Task DeleteTracksItem(string id)
+        {
+            return DeleteAsync(id);
+        }
+    }
+}
---
+++ CloudService/JKApplicationService/DataObjects/TracksItem.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Azure.Mobile.Server;
+using Microsoft.OData.Edm;
+
+namespace JKApplicationService.DataObjects
+{
+    public class TracksItem : EntityData
+    {
+        private float distance { get; set; }
+
+        private Date travelTime { get; set; }
+
+        private Date finishTime { get; set; }
+
+        private int calBurned { get; set; }
+
+        private string travellingPoints { get; set; }
+    }
+}
